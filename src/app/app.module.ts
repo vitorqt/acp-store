5407604e827a6ea2672e2ceff2c76b87fb87e7c5
Vitor Rodrigues <vitorqt@hotmail.com>
2018-10-18 11:05:38 -0300

    Adiciona páginas de usuário

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { UsersComponent } from './pages/users/users.component';
+import { SuportComponent } from './pages/suport/suport.component';
+import { RegisterComponent } from './pages/register/register.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'suport', component: SuportComponent },
+  { path: 'register', component: RegisterComponent}
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    HomeComponent,
+    UsersComponent,
+    SuportComponent,
+    RegisterComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
     BrowserModule
   ],
   providers: [],
